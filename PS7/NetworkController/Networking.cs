a4fe2f25e0d768b33fa43ca224e2f34ee42032ad
scottskidmore <scottytskidmore@gmail.com>
2023-11-05 13:19:40 -0700

    fix 1

--- PS7/NetworkController/Networking.cs
+++ PS7/NetworkController/Networking.cs
@@ -50,7 +50,7 @@ public static class Networking
     /// 1) a delegate so the user can take action (a SocketState Action), and 2) the TcpListener</param>
     private static void AcceptNewClient(IAsyncResult ar)
     {
-        Tuple<Action<SocketState>, TcpListener> items = (Tuple<Action<SocketState>, TcpListener>)ar;
+        Tuple<Action<SocketState>, TcpListener> items = (Tuple<Action<SocketState>, TcpListener>)ar.AsyncState!;
         Socket newClient =items.Item2.EndAcceptSocket(ar);
         SocketState state = new SocketState(items.Item1,newClient);
         state.OnNetworkAction(state);
@@ -147,6 +147,7 @@ public static class Networking
         // Connect
         try
         {
+            Console.WriteLine(ipAddress);
             state.TheSocket.BeginConnect(ipAddress, port, ConnectedCallback, state);
         }
         catch (Exception e)
@@ -185,6 +186,7 @@ public static class Networking
 
             toCall.Invoke(state);
         }
+        state.OnNetworkAction(state);
     }
 
 
@@ -254,8 +256,7 @@ public static class Networking
         }
         SocketState state = new SocketState((SocketState obj) => Task.Delay(0), socket);
         byte[] messageBytes = Encoding.UTF8.GetBytes(data);
-        socket.BeginSend(messageBytes, 0,
-        state.buffer.Length, SocketFlags.None, SendCallback, state);
+        socket.BeginSend(messageBytes, 0,messageBytes.Length, SocketFlags.None, SendCallback, state);
         return true;
     }
 
@@ -274,8 +275,6 @@ public static class Networking
     {
         SocketState state = (SocketState)ar.AsyncState!;
         state.TheSocket.EndSend(ar);
-        state.TheSocket.BeginSend(state.buffer, 0,
-        state.buffer.Length, SocketFlags.None, SendCallback, state);
     }
 
 
