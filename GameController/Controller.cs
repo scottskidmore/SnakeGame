a3361217b707576e7b9f228122bb45bac0dad254
scottskidmore <scottytskidmore@gmail.com>
2023-11-20 14:15:19 -0700

    message length fix

--- GameController/Controller.cs
+++ GameController/Controller.cs
@@ -4,6 +4,7 @@ using System.Net.Http.Json;
 using System.Net.Sockets;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 using World;
 
 namespace GameController
@@ -82,25 +83,35 @@ namespace GameController
                 return;
             }
             string data = state.GetData();
-            string[] list = data.Split("\n");
+            string[] list = Regex.Split(data, @"(?<=[\n])");
             foreach (string s in list)
             {
                 if (s.StartsWith("{"))
                 {
+                    // Ignore empty strings added by the regex splitter
+                    if (s.Length == 0)
+                        continue;
+                    // The regex splitter will include the last string even if it doesn't end with a '\n',
+                    // So we need to ignore it if this happens. 
+                    if (s[s.Length - 1] != '\n')
+                        break;
+
+                    // Then remove it from the SocketState's growable buffer
+                    state.RemoveData(0, s.Length);
                     JsonDocument doc = JsonDocument.Parse(s);
                     if (doc.RootElement.TryGetProperty("wall", out _))
                     {
-                        World.Wall wall = JsonSerializer.Deserialize<World.Wall>(s);
+                        Wall wall = JsonSerializer.Deserialize<World.Wall>(s);
                         world.Walls.Add(wall);
                     }
                     if (doc.RootElement.TryGetProperty("power", out _))
                     {
-                        World.PowerUp power = JsonSerializer.Deserialize<World.PowerUp>(s);
+                        PowerUp power = JsonSerializer.Deserialize<World.PowerUp>(s);
                         world.PowerUps.Add(power);
                     }
                     if (doc.RootElement.TryGetProperty("snake", out _))
                     {
-                        World.Snake snake = JsonSerializer.Deserialize<World.Snake>(s);
+                        Snake snake = JsonSerializer.Deserialize<World.Snake>(s);
                         world.Snakes.Add(snake);
                     }
                 }
