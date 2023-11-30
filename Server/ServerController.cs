657efe1187516a8e5a985409adb996ee9c6e88dd
u0926460 <71471282+u0926460@users.noreply.github.com>
2023-11-30 13:53:12 -0700

    Stuff i worked on in class

--- Server/ServerController.cs
+++ Server/ServerController.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Serialization;
 using NetworkUtil;
@@ -120,8 +121,27 @@ namespace Server
             // change the state's network action to the 
             // receive handler so we can process data when something
             // happens on the network
-            
+            state.OnNetworkAction = ReceiveHandshake;
 
+            Networking.GetData(state);
+        }
+
+        /// <summary>
+        /// Method to be invoked by the networking library
+        /// when a network action occurs (see lines 64-66)
+        /// </summary>
+        /// <param name="state"></param>
+        private void ReceiveHandshake(SocketState state)
+        {
+            // Remove the client if they aren't still connected
+            if (state.ErrorOccurred)
+            {
+                RemoveClient(state.ID);
+                return;
+            }
+
+            ProcessMessage(state);
+            // Continue the event loop that receives messages from this client
             Networking.GetData(state);
         }
 
@@ -131,6 +151,74 @@ namespace Server
             //Handle the command from client
            // Networking.SendAndClose(state.TheSocket, response);
         }
+
+        /// <summary>
+        /// Given the data that has arrived so far, 
+        /// potentially from multiple receive operations, 
+        /// determine if we have enough to make a complete message,
+        /// and process it (print it and broadcast it to other clients).
+        /// </summary>
+        /// <param name="sender">The SocketState that represents the client</param>
+        private void ProcessMessage(SocketState state)
+        {
+            string totalData = state.GetData();
+
+            string[] parts = Regex.Split(totalData, @"(?<=[\n])");
+
+            // Loop until we have processed all messages.
+            // We may have received more than one.
+            foreach (string p in parts)
+            {
+                // Ignore empty strings added by the regex splitter
+                if (p.Length == 0)
+                    continue;
+                // The regex splitter will include the last string even if it doesn't end with a '\n',
+                // So we need to ignore it if this happens. 
+                if (p[p.Length - 1] != '\n')
+                    break;
+
+                
+
+                // Remove it from the SocketState's growable buffer
+                state.RemoveData(0, p.Length);
+
+                //if we recieved a json moving
+                if (p.StartsWith("{"))
+                {
+
+                }
+
+                // Broadcast the message to all clients
+                // Lock here beccause we can't have new connections 
+                // adding while looping through the clients list.
+                // We also need to remove any disconnected clients.
+                HashSet<long> disconnectedClients = new HashSet<long>();
+                lock (clients)
+                {
+                    foreach (SocketState client in clients.Values)
+                    {
+                        if (!Networking.Send(client.TheSocket!, "Message from client " + state.ID + ": " + p))
+                            disconnectedClients.Add(client.ID);
+                    }
+                }
+                foreach (long id in disconnectedClients)
+                    RemoveClient(id);
+            }
+        }
+
+
+        /// <summary>
+        /// Removes a client from the clients dictionary
+        /// </summary>
+        /// <param name="id">The ID of the client</param>
+        private void RemoveClient(long id)
+    {
+        Console.WriteLine("Client " + id + " disconnected");
+        lock (clients)
+        {
+            clients.Remove(id);
+        }
+    }
     }
 }
 
